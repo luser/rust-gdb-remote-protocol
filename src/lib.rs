aed89897e82d82a236abf318a83116120b55e359
Ted Mielczarek <ted@mielczarek.org>
2015-11-21 16:09:23 -0500

    add consumer

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@ named!(checksum<&[u8], u8>,
 
 named!(packet<&[u8], (Vec<u8>, u8)>,
        preceded!(tag!("$"),
-                 separated_pair!(map!(take_until!("#"), |s : &[u8]| s.to_vec()),
+                 separated_pair!(map!(opt!(is_not!("#")), |o : Option<&[u8]>| o.map_or(vec!(), |s| s.to_vec())),
                                  tag!("#"),
                                  checksum)));
 
@@ -29,6 +29,8 @@ named!(packet_or_response<Packet>, alt!(
     | tag!("-") => { |_|   Packet::Nack }
     ));
 
+consumer_from_parser!(GdbConsumer<Packet>, packet_or_response);
+
 #[test]
 fn test_checksum() {
     assert_eq!(checksum(&b"00"[..]), Done(&b""[..], 0));
@@ -39,12 +41,18 @@ fn test_checksum() {
 
 #[test]
 fn test_packet() {
+    use nom::Needed;
     assert_eq!(packet(&b"$#00"[..]), Done(&b""[..], (b""[..].to_vec(), 0)));
     assert_eq!(packet(&b"$xyz#00"[..]), Done(&b""[..], (b"xyz"[..].to_vec(), 0)));
     assert_eq!(packet(&b"$a#a1"[..]), Done(&b""[..], (b"a"[..].to_vec(), 0xa1)));
     assert_eq!(packet(&b"$foo#ffxyz"[..]), Done(&b"xyz"[..], (b"foo"[..].to_vec(), 0xff)));
     assert_eq!(packet(&b"$qSupported:multiprocess+;xmlRegisters=i386;qRelocInsn+#b5"[..]),
                Done(&b""[..], (b"qSupported:multiprocess+;xmlRegisters=i386;qRelocInsn+"[..].to_vec(), 0xb5)));
+    assert_eq!(packet(&b"$"[..]), Incomplete(Needed::Size(1)));
+    assert_eq!(packet(&b"$#"[..]), Incomplete(Needed::Size(2)));
+    assert_eq!(packet(&b"$xyz"[..]), Incomplete(Needed::Size(1)));
+    assert_eq!(packet(&b"$xyz#"[..]), Incomplete(Needed::Size(2)));
+    assert_eq!(packet(&b"$xyz#a"[..]), Incomplete(Needed::Size(2)));
 }
 
 #[test]
@@ -52,4 +60,87 @@ fn test_packet_or_response() {
     assert_eq!(packet_or_response(&b"$#00"[..]), Done(&b""[..], Packet::Data(b""[..].to_vec(), 0)));
     assert_eq!(packet_or_response(&b"+"[..]), Done(&b""[..], Packet::Ack));
     assert_eq!(packet_or_response(&b"-"[..]), Done(&b""[..], Packet::Nack));
+}
+
+#[test]
+fn test_consumer() {
+    let mut c = GdbConsumer::new();
+    {
+        let state = c.state();
+        if let &ConsumerState::Continue(Move::Consume(n)) = state {
+            assert_eq!(n, 0);
+        } else {
+            assert!(false, format!("Bad state: {:?}", state));
+        }
+    }
+    {
+        let state = c.handle(Input::Empty);
+        if let &ConsumerState::Continue(Move::Consume(n)) = state {
+            assert_eq!(n, 0);
+        } else {
+            assert!(false, format!("Bad state: {:?}", state));
+        }
+    }
+    {
+        let state = c.handle(Input::Element(&b"+"[..]));
+        if let &ConsumerState::Done(Move::Consume(n),
+                                    Packet::Ack) = state {
+            assert_eq!(n, 1);
+        } else {
+            assert!(false, format!("Bad state: {:?}", state));
+        }
+    }
+    {
+        let state = c.handle(Input::Element(&b"-"[..]));
+        if let &ConsumerState::Done(Move::Consume(n),
+                                    Packet::Nack) = state {
+            assert_eq!(n, 1);
+        } else {
+            assert!(false, format!("Bad state: {:?}", state));
+        }
+    }
+    {
+        let state = c.handle(Input::Element(&b"$#00"[..]));
+        if let &ConsumerState::Done(Move::Consume(n),
+                                    Packet::Data(ref d, 0)) = state {
+            assert_eq!(n, 4);
+            assert_eq!(*d, b""[..].to_vec());
+        } else {
+            assert!(false, format!("Bad state: {:?}", state));
+        }
+    }
+    {
+        let state = c.handle(Input::Element(&b"$xyz#a1"[..]));
+        if let &ConsumerState::Done(Move::Consume(n),
+                                    Packet::Data(ref d, 0xa1)) = state {
+            assert_eq!(n, 7);
+            assert_eq!(*d, b"xyz"[..].to_vec());
+        } else {
+            assert!(false, format!("Bad state: {:?}", state));
+        }
+    }
+    {
+        let state = c.handle(Input::Element(&b"$xyz"[..]));
+        if let &ConsumerState::Continue(Move::Await(_)) = state {
+            assert!(true, "OK!");
+        } else {
+            assert!(false, format!("Bad state: {:?}", state));
+        }
+    }
+    {
+        let state = c.handle(Input::Element(&b"$xyz#"[..]));
+        if let &ConsumerState::Continue(Move::Await(_)) = state {
+            assert!(true, "OK!");
+        } else {
+            assert!(false, format!("Bad state: {:?}", state));
+        }
+    }
+    {
+        let state = c.handle(Input::Element(&b"$xyz#a"[..]));
+        if let &ConsumerState::Continue(Move::Await(_)) = state {
+            assert!(true, "OK!");
+        } else {
+            assert!(false, format!("Bad state: {:?}", state));
+        }
+    }
 }
