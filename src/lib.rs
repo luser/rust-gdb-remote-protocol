ff559020e86a0da3fe9522773e37e83329248164
Ted Mielczarek <ted@mielczarek.org>
2017-10-10 10:08:46 -0400

    Update nom and add support for parsing qSupported and some other bits

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 #[macro_use]
 extern crate nom;
+extern crate strum;
+#[macro_use]
+extern crate strum_macros;
 
 use nom::IResult::*;
-use nom::IResult;
-use std::collections::HashMap;
+use nom::{IResult, Needed};
 use std::io::{self,BufRead,BufReader,Read,Write};
-use std::str;
+use std::str::{self, FromStr};
 
 
 named!(checksum<&[u8], u8>,
@@ -14,7 +16,9 @@ named!(checksum<&[u8], u8>,
 
 named!(packet<&[u8], (Vec<u8>, u8)>,
        preceded!(tag!("$"),
-                 separated_pair!(map!(opt!(is_not!("#")), |o : Option<&[u8]>| o.map_or(vec!(), |s| s.to_vec())),
+                 separated_pair!(map!(opt!(is_not!("#")), |o: Option<&[u8]>| {
+                     o.map_or(vec!(), |s| s.to_vec())
+                 }),
                                  tag!("#"),
                                  checksum)));
 
@@ -31,13 +35,59 @@ named!(packet_or_response<Packet>, alt!(
     | tag!("-") => { |_|   Packet::Nack }
     ));
 
+#[allow(non_camel_case_types)]
+#[derive(Copy, Clone, Debug, EnumString, PartialEq)]
+enum GDBFeature {
+    multiprocess,
+    xmlRegisters,
+    qRelocInsn,
+    swbreak,
+    hwbreak,
+    #[strum(serialize="fork-events")]
+    fork_events,
+    #[strum(serialize="vfork-events")]
+    vfork_events,
+    #[strum(serialize="exec-events")]
+    exec_events,
+    vContSupported,
+    // these are not listed in the docs but GDB sends them
+    #[strum(serialize="no-resumed")]
+    no_resumed,
+    QThreadEvents,
+}
+
+#[derive(Debug, PartialEq)]
+enum Known<'a> {
+    Yes(GDBFeature),
+    No(&'a str),
+}
+
+#[derive(Debug, PartialEq)]
+struct GDBFeatureSupported<'a>(Known<'a>, FeatureSupported<'a>);
+
+#[derive(Debug, PartialEq)]
 enum FeatureSupported<'a> {
     Yes,
     No,
+    #[allow(unused)]
     Maybe,
     Value(&'a str),
 }
-/// GDB remote protocol commands, as defined in https://sourceware.org/gdb/onlinedocs/gdb/Packets.html#Packets
+
+#[derive(Debug, PartialEq)]
+enum Query<'a> {
+    /// Return the current thread ID.
+    CurrentThread,
+    /// Compute the CRC checksum of a block of memory.
+    #[allow(unused)]
+    CRC { addr: u64, length: u64 },
+    /// Tell the remote stub about features supported by gdb, and query the stub for features
+    /// it supports.
+    SupportedFeatures(Vec<GDBFeatureSupported<'a>>),
+}
+
+/// GDB remote protocol commands, as defined in (the GDB documentation)[1]
+/// [1]: https://sourceware.org/gdb/onlinedocs/gdb/Packets.html#Packets
 enum Command<'a> {
     /// Enable extended mode.
     EnableExtendedMode,
@@ -49,12 +99,57 @@ enum Command<'a> {
     ReadGeneralRegisters,
     // Kill request.
     Kill,
-    /// Tell the remote stub about features supported by gdb, and query the stub for features it supports.
-    QuerySupportedFeatures(HashMap<&'a str, FeatureSupported<'a>>),
+    Query(Query<'a>),
     Reset,
 }
 
-fn command<'a>(i : &'a [u8]) -> IResult<&'a [u8], Command<'a>> {
+named!(gdbfeature<Known>, map!(map_res!(is_not_s!(";="), str::from_utf8), |s| {
+    match GDBFeature::from_str(s) {
+        Ok(f) => Known::Yes(f),
+        Err(_) => Known::No(s),
+    }
+}));
+
+fn gdbfeaturesupported<'a>(i: &'a [u8]) -> IResult<&'a [u8], GDBFeatureSupported<'a>> {
+    flat_map!(i, is_not!(";"), |f: &'a [u8]| {
+        match f.split_last() {
+            None => IResult::Incomplete(Needed::Size(2)),
+            Some((&b'+', first)) => {
+                map!(first, gdbfeature, |feat| GDBFeatureSupported(feat, FeatureSupported::Yes))
+            }
+            Some((&b'-', first)) => {
+                map!(first, gdbfeature, |feat| GDBFeatureSupported(feat, FeatureSupported::No))
+            }
+            Some((_, _)) => {
+                map!(f, separated_pair!(gdbfeature, tag!("="),
+                                        map_res!(is_not!(";"), str::from_utf8)),
+                     |(feat, value)| GDBFeatureSupported(feat, FeatureSupported::Value(value)))
+            }
+        }
+    })
+}
+
+fn query<'a>(i: &'a [u8]) -> IResult<&'a [u8], Query<'a>> {
+    preceded!(i, tag!("q"),
+              alt_complete!(
+                  tag!("C") => { |_| Query::CurrentThread }
+                  /*
+                  | preceded!(tag!("CRC"), separated_pair!(addr, tag!(","), length)) => {
+                  |(addr, length)| Query::CRC { addr, length }
+              }
+                   */
+                  | preceded!(tag!("Supported"),
+                              preceded!(tag!(":"),
+                                        separated_list_complete!(tag!(";"),
+                                                                 gdbfeaturesupported))) => {
+                      |features: Vec<GDBFeatureSupported<'a>>| Query::SupportedFeatures(features)
+                  }
+                  )
+              )
+}
+
+
+fn command<'a>(i: &'a [u8]) -> IResult<&'a [u8], Command<'a>> {
     alt!(i,
     tag!("!") => { |_|   Command::EnableExtendedMode }
     | tag!("?") => { |_| Command::TargetHaltReason }
@@ -79,6 +174,7 @@ fn command<'a>(i : &'a [u8]) -> IResult<&'a [u8], Command<'a>> {
     // p n
     // P n...=r...
     // ‘q name params...’
+    | query => { |q| Command::Query(q) }
     // ‘Q name params...’
     | tag!("r") => { |_| Command::Reset }
     | preceded!(tag!("R"), take!(2)) => { |_| Command::Reset }
@@ -104,34 +200,52 @@ fn command<'a>(i : &'a [u8]) -> IResult<&'a [u8], Command<'a>> {
 }
 
 pub trait Handler {
+    fn query_supported_features() {}
 }
 
 /// Compute a checksum of `bytes`: modulo-265 sum of each byte in `bytes`.
-fn compute_checksum(bytes : &[u8]) -> u8 {
+fn compute_checksum(bytes: &[u8]) -> u8 {
     bytes.iter().fold(0, |sum, &b| sum.wrapping_add(b))
 }
 
+fn unsupported<W>(writer: &mut W) -> io::Result<()>
+    where W: Write,
+{
+    writer.write_all(&b"$#00"[..])
+}
+
 /// Handle a single packet `data` with `handler` and write a response to `writer`.
-fn handle_packet<H : Handler, W : Write>(data : &[u8],
-                                         _handler : &H,
-                                         writer : &mut W) -> io::Result<()> {
+fn handle_packet<H, W>(data : &[u8],
+                       _handler : &H,
+                       writer : &mut W) -> io::Result<()>
+    where H: Handler,
+W: Write,
+{
     println!("Command: {}", str::from_utf8(data).unwrap());
     if let Done(_, ref command) = command(data) {
-        writer.write_all(&b"$#00"[..])
+        match *command {
+            Command::EnableExtendedMode => unimplemented!(),
+            Command::TargetHaltReason => unimplemented!(),
+            Command::ToggleDebug => unimplemented!(),
+            Command::ReadGeneralRegisters => unimplemented!(),
+            Command::Kill => unimplemented!(),
+            Command::Reset => unimplemented!(),
+            _ => unsupported(writer),
+        }
     } else {
         // Return an empty response for unsupported commands.
-        writer.write_all(&b"$#00"[..])
+        unsupported(writer)
     }
 }
 
-fn offset(from : &[u8], to : &[u8]) -> usize {
+fn offset(from: &[u8], to: &[u8]) -> usize {
     let fst = from.as_ptr();
     let snd = to.as_ptr();
 
     snd as usize - fst as usize
 }
 
-fn run_parser(buf : &[u8]) -> Option<(usize, Packet)> {
+fn run_parser(buf: &[u8]) -> Option<(usize, Packet)> {
     if let Done(rest, packet) = packet_or_response(buf) {
         Some((offset(buf, rest), packet))
     } else {
@@ -139,10 +253,15 @@ fn run_parser(buf : &[u8]) -> Option<(usize, Packet)> {
     }
 }
 
-/// Read gdbserver packets from `reader` and call methods on `handler` to handle them and write responses to `writer`.
-pub fn process_packets_from<R, W, H>(reader : R,
-                                     mut writer : W,
-                                     handler : H) where R : Read, W : Write, H : Handler {
+/// Read gdbserver packets from `reader` and call methods on `handler` to handle them and write
+/// responses to `writer`.
+pub fn process_packets_from<R, W, H>(reader: R,
+                                     mut writer: W,
+                                     handler: H)
+    where R: Read,
+W: Write,
+H: Handler
+{
     let mut bufreader = BufReader::new(reader);
     let mut done = false;
     while !done {
@@ -188,7 +307,8 @@ pub fn process_packets_from<R, W, H>(reader : R,
 #[test]
 fn test_compute_checksum() {
     assert_eq!(compute_checksum(&b""[..]), 0);
-    assert_eq!(compute_checksum(&b"qSupported:multiprocess+;xmlRegisters=i386;qRelocInsn+"[..]), 0xb5);
+    assert_eq!(compute_checksum(&b"qSupported:multiprocess+;xmlRegisters=i386;qRelocInsn+"[..]),
+               0xb5);
 }
 
 #[test]
@@ -207,12 +327,14 @@ fn test_packet() {
     assert_eq!(packet(&b"$a#a1"[..]), Done(&b""[..], (b"a"[..].to_vec(), 0xa1)));
     assert_eq!(packet(&b"$foo#ffxyz"[..]), Done(&b"xyz"[..], (b"foo"[..].to_vec(), 0xff)));
     assert_eq!(packet(&b"$qSupported:multiprocess+;xmlRegisters=i386;qRelocInsn+#b5"[..]),
-               Done(&b""[..], (b"qSupported:multiprocess+;xmlRegisters=i386;qRelocInsn+"[..].to_vec(), 0xb5)));
-    assert_eq!(packet(&b"$"[..]), Incomplete(Needed::Size(1)));
-    assert_eq!(packet(&b"$#"[..]), Incomplete(Needed::Size(2)));
-    assert_eq!(packet(&b"$xyz"[..]), Incomplete(Needed::Size(1)));
-    assert_eq!(packet(&b"$xyz#"[..]), Incomplete(Needed::Size(2)));
-    assert_eq!(packet(&b"$xyz#a"[..]), Incomplete(Needed::Size(2)));
+               Done(&b""[..],
+                    (b"qSupported:multiprocess+;xmlRegisters=i386;qRelocInsn+"[..].to_vec(),
+                     0xb5)));
+    assert_eq!(packet(&b"$"[..]), Incomplete(Needed::Size(2)));
+    assert_eq!(packet(&b"$#"[..]), Incomplete(Needed::Size(4)));
+    assert_eq!(packet(&b"$xyz"[..]), Incomplete(Needed::Size(5)));
+    assert_eq!(packet(&b"$xyz#"[..]), Incomplete(Needed::Size(7)));
+    assert_eq!(packet(&b"$xyz#a"[..]), Incomplete(Needed::Size(7)));
 }
 
 #[test]
@@ -222,5 +344,63 @@ fn test_packet_or_response() {
     assert_eq!(packet_or_response(&b"-"[..]), Done(&b""[..], Packet::Nack));
 }
 
-//     let bytes = b"+$qSupported:multiprocess+;xmlRegisters=i386;qRelocInsn+#b5$qSupported:multiprocess+;xmlRegisters=i386;qRelocInsn+#b5-";
+#[test]
+fn test_gdbfeaturesupported() {
+    assert_eq!(gdbfeaturesupported(&b"multiprocess+"[..]),
+               Done(&b""[..], GDBFeatureSupported(Known::Yes(GDBFeature::multiprocess),
+                                                  FeatureSupported::Yes)));
+    assert_eq!(gdbfeaturesupported(&b"xmlRegisters=i386"[..]),
+               Done(&b""[..], GDBFeatureSupported(Known::Yes(GDBFeature::xmlRegisters),
+                                                  FeatureSupported::Value("i386"))));
+    assert_eq!(gdbfeaturesupported(&b"qRelocInsn-"[..]),
+               Done(&b""[..], GDBFeatureSupported(Known::Yes(GDBFeature::qRelocInsn),
+                                                  FeatureSupported::No)));
+    assert_eq!(gdbfeaturesupported(&b"vfork-events+"[..]),
+               Done(&b""[..], GDBFeatureSupported(Known::Yes(GDBFeature::vfork_events),
+                                                  FeatureSupported::Yes)));
+    assert_eq!(gdbfeaturesupported(&b"vfork-events-"[..]),
+               Done(&b""[..], GDBFeatureSupported(Known::Yes(GDBFeature::vfork_events),
+                                                  FeatureSupported::No)));
+    assert_eq!(gdbfeaturesupported(&b"unknown-feature+"[..]),
+               Done(&b""[..], GDBFeatureSupported(Known::No("unknown-feature"),
+                                                  FeatureSupported::Yes)));
+    assert_eq!(gdbfeaturesupported(&b"unknown-feature-"[..]),
+               Done(&b""[..], GDBFeatureSupported(Known::No("unknown-feature"),
+                                                  FeatureSupported::No)));
+}
+
+#[test]
+fn test_gdbfeature() {
+    assert_eq!(gdbfeature(&b"multiprocess"[..]),
+               Done(&b""[..], Known::Yes(GDBFeature::multiprocess)));
+    assert_eq!(gdbfeature(&b"fork-events"[..]),
+               Done(&b""[..], Known::Yes(GDBFeature::fork_events)));
+    assert_eq!(gdbfeature(&b"some-unknown-feature"[..]),
+               Done(&b""[..], Known::No("some-unknown-feature")));
+}
+
+#[test]
+fn test_query() {
+    // From a gdbserve packet capture.
+    let b = concat!("qSupported:multiprocess+;swbreak+;hwbreak+;qRelocInsn+;fork-events+;",
+                    "vfork-events+;exec-events+;vContSupported+;QThreadEvents+;no-resumed+;",
+                    "xmlRegisters=i386");
+    assert_eq!(query(b.as_bytes()),
+               Done(&b""[..], Query::SupportedFeatures(vec![
+                   GDBFeatureSupported(Known::Yes(GDBFeature::multiprocess), FeatureSupported::Yes),
+                   GDBFeatureSupported(Known::Yes(GDBFeature::swbreak), FeatureSupported::Yes),
+                   GDBFeatureSupported(Known::Yes(GDBFeature::hwbreak), FeatureSupported::Yes),
+                   GDBFeatureSupported(Known::Yes(GDBFeature::qRelocInsn), FeatureSupported::Yes),
+                   GDBFeatureSupported(Known::Yes(GDBFeature::fork_events), FeatureSupported::Yes),
+                   GDBFeatureSupported(Known::Yes(GDBFeature::vfork_events), FeatureSupported::Yes),
+                   GDBFeatureSupported(Known::Yes(GDBFeature::exec_events), FeatureSupported::Yes),
+                   GDBFeatureSupported(Known::Yes(GDBFeature::vContSupported),
+                                       FeatureSupported::Yes),
+                   GDBFeatureSupported(Known::Yes(GDBFeature::QThreadEvents),
+                                       FeatureSupported::Yes),
+                   GDBFeatureSupported(Known::Yes(GDBFeature::no_resumed), FeatureSupported::Yes),
+                   GDBFeatureSupported(Known::Yes(GDBFeature::xmlRegisters),
+                                       FeatureSupported::Value("i386")),
+                   ])));
+}
 
